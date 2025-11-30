40bf94ce6b0d9ca8fbbd27853a0b062a0fa50905
ChimaBright <blakechima@gmail.com>
2025-11-30 17:31:14 +0100

    StatsCard and my Listing Page

--- client/src/pages/MyListing.jsx
+++ client/src/pages/MyListing.jsx
@@ -1,8 +1,192 @@
 import React from 'react'
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {
+    ArrowDownCircleIcon,
+    CheckCircle,
+    CoinsIcon,
+    DollarSign,
+    Eye, Lock,
+    PlusIcon, StarIcon,
+    TrendingUp, UserIcon,
+    WalletIcon
+} from "lucide-react";
+import StatsCard from "../components/StatsCard.jsx";
+import {platformIcons} from "../assets/assets.jsx";
 
 const MyListing = () => {
+    const {userListings, balance} = useSelector((state) => state.listing);
+
+    const currency = import.meta.env.VITE_CURRENCY || '$';
+    const navigate = useNavigate();
+
+    const totalValue = userListings.reduce((sum, listing) => sum + (listing.price || 0), 0);
+    const activeListings = userListings.filter((listing) => listing.status === 'active').length;
+    const soldListings = userListings.filter((listing) => listing.status === 'sold').length;
+
+    const numberFormat = (num) => {
+        if(num >=1000000) return (num/1000000).toFixed(1)+'M';
+        if(num >=1000) return (num/1000).toFixed(1)+'K';
+        return num?.toLocaleString() || "0"
+    }
+
     return (
-        <div>MyListing</div>
+        <div className='px-6 md:px-16 lg:px-24 xl:px-32 pt-8'>
+        {/* Header */}
+            <div className='flex flex-col md:flex-row  justify-between items-start md:items-center mb-8'>
+                <div>
+                    <h1 className='text-3xl font-bold text-gray-800'>My Listings</h1>
+                    <p className='text-gray-600 mt-1'>Manage your social media account listing</p>
+                </div>
+
+                <button onClick={()=> navigate('/create-listing')} className='bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded font-medium flex items-center space-x-2 md:mt-0'>
+                    <PlusIcon className='w-6 h-6 ' />
+                    <span className=''>My Listing</span>
+                </button>
+            </div>
+
+        {/*  Stats */}
+            <div className='grid grid-cols-1 md:grid-cols-4 mb-8 gap-6'>
+                <StatsCard
+                    title='Total Listings'
+                    value={userListings.length}
+                    color='indigo'
+                    icon={<Eye className='w-6 h-6 text-indigo-600' />}
+                />
+
+                <StatsCard
+                    title='Active Listings'
+                    value={activeListings}
+                    color='green'
+                    icon={<CheckCircle className='w-6 h-6 text-green-600' />}
+                />
+
+                <StatsCard
+                    title='Sold'
+                    value={soldListings}
+                    color='indigo'
+                    icon={<TrendingUp className='w-6 h-6 text-indigo-600' />}
+                />
+
+                <StatsCard
+                    title='Total Value'
+                    value={`${currency}${totalValue.toLocaleString()}`}
+                    color='yellow'
+                    icon={<DollarSign className='w-6 h-6 text-yellow-600' />}
+                />
+            </div>
+
+            {/* Balance Section */}
+            <div className='flex flex-col sm:flex-row justify-between gap-4 xl:gap-20 p-6 mb-10 bg-white rounded-xl border border-gray-200'>
+                {[
+                    {label: 'Earned', value: balance.earned, icon: WalletIcon},
+                    {label: 'Withdrawn', value: balance.withdrawn, icon: ArrowDownCircleIcon},
+                    {label: 'Available', value: balance.available, icon: CoinsIcon},
+                ].map((item, index) => (
+                    <div key={index} className='flex flex-1 items-center justify-between p-4 rounded-lg border border-gray-100 cursor-pointer'>
+                        <div className='flex items-center gap-3'>
+                            <item.icon className='w-6 h-6 text-gray-500'/>
+                            <span className='text-gray-600 font-medium'>{item.label}</span>
+                        </div>
+
+                        <span className='text-xl font-medium text-gray-700'>
+                            {currency}
+                            {item.value.toFixed(2)}
+                        </span>
+                    </div>
+                ))}
+            </div>
+
+            {/* Listings */}
+            {userListings.length === 0 ? (
+                <div className='bg-white rounded-lg border border-gray-200 p-16 text-center'>
+                    <div className='w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center mx-auto mb-4'>
+                        <PlusIcon className='w-8 h-8 text-gray-400' />
+                    </div>
+                    <h3 className='text-xl font-medium mb-2 text-gray-800'>No listing yet</h3>
+                    <p className='text-gray-600 mb-6'>Start by creating your first listing</p>
+                    <button
+                        onClick={()=> navigate('/create-listing')}
+                        className='bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg font-medium'
+                    >
+                        Create First Listing
+                    </button>
+                </div>
+            ) : (
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    {userListings.map((listing) => (
+                        <div key={listing.id} className='bg-white rounded-lg border border-gray-200 hover:shadow-lg shadow-gray-200/70 transition-shadow'>
+                            <div className='p-6'>
+                                <div className='flex items-start gap-4 justify-between mb-4'>
+                                    {platformIcons[listing.platform]}
+                                    <div className='flex-1'>
+                                        <div className='flex items-start justify-between'>
+                                            <h3 className='text-lg font-semibold text-gray-800'>{listing.title}</h3>
+                                            <div className='flex items-center gap-2'>
+                                                    <div className='relative group'>
+                                                        <Lock size={14} />
+                                                        <div className='invisible group-hover:visible absolute right-0 top-0 z-10 pt-4.5'>
+                                                            <div className='bg-white text-gray-600 text-xs rounded boder border-gray-200 p-2 px-3'>
+                                                                {!listing.isCredentialSubmitted && (
+                                                                    <>
+                                                                        <button className='flex items-center gap-2 text-nowrap'>
+                                                                            Add Credentials
+                                                                        </button>
+                                                                        <hr className='my-2 border-gray-200' />
+                                                                    </>
+                                                                )}
+
+                                                                <button className='text-nowrap'>
+                                                                    Status: {" "}
+                                                                    <span className={listing.isCredentialSubmitted
+                                                                            ? listing.isCredentialVerified
+                                                                                ? listing.isCredentialChanged ? "text-green-600" : "text-indigo-600" : "text-slate-600" : "text-red-600"}>
+                                                                        {listing.isCredentialSubmitted
+                                                                            ? listing.isCredentialVerified
+                                                                                ? listing.isCredentialChanged
+                                                                                    ? "Changed"
+                                                                                    : "Verified"
+                                                                                : "Submitted"
+                                                                            : "Not Submitted"
+                                                                        }
+                                                                    </span>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {listing.status === 'active' && (
+                                                        <StarIcon
+                                                            size={18}
+                                                            className={`text-yellow-500 cursor-pointer ${listing.featured && 'fill-yellow-500'}`}
+                                                        />
+                                                    )}
+                                            </div>
+                                        </div>
+
+                                        <p className='text-gray-600 text-sm'>
+                                            <span>@{listing.username}</span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className='space-y-4'>
+                                    <div className='grid grid-cols-2 gap-4 text-sm'>
+                                        <div className='flex items-center space-x-2'>
+                                            <UserIcon className='w-5 h-5 text-gray-400' />
+                                            <span>{numberFormat(listing.followers_count)} followers</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {/*    */}
+            {/*    */}
+
+        </div>
     )
 }
 export default MyListing
