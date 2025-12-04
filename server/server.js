b5394f7d44a774c799a08e68b270fbf32f61b469
ChimaBright <blakechima@gmail.com>
2025-12-04 20:35:09 +0100

    setting up inngest, clerk auth and also prisma schema

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
-import "dotenv/config";
+import "dotenv/config"
 import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
-app.get('/', (req, res) => res.send('Server Is Live!'));
+app.get('/', (req, res) => res.send('Server is running!'));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 3000;
 
